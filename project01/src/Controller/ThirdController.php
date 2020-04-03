a9e15f0e26637c52fed7008361b41db060f2b6e5
Miki <63016469+mdjordjevic86@users.noreply.github.com>
2020-04-03 18:28:12 +0200

    Added ThirdController.php to test twig template engine and passing vars to templates

---
+++ project01/src/Controller/ThirdController.php
@@ -0,0 +1,34 @@
+<?php
+	/**
+	 * Class ThirdController
+	 *
+	 * @package App\Controller
+	 * @author  Milan Djordjevic <mdjordjevic@jaggaer.com>
+	 *
+	 * @date    : 4/3/20
+	 * @time    : 6:21 PM
+	 */
+
+	namespace App\Controller;
+
+
+	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+	use Symfony\Component\Routing\Annotation\Route;
+
+	class ThirdController extends AbstractController {
+
+		/**
+		 * Test passing vars to twig template engine
+		 * @Route()
+		 */
+		public function show() {
+			$debugText = "This is some text to test passing vars to twig template engine";
+
+			return $this->render(
+				'third/show.html.twig',
+				[
+					'testText' => $debugText
+				]
+			);
+		}
+	}
